2a96a3688df8ff86b708c93f59b85978f495b9ba
JBT <ADMIN@JAVABEGINNERSTUTORIAL.COM>
2021-11-06 23:07:05 +0530

    initial commit

---
+++ src/main/java/com/jbt/springbootwrapperold/SpringBootWrapperOldApplication.java
@@ -0,0 +1,13 @@
+package com.jbt.springbootwrapperold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWrapperOldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWrapperOldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jbt/springbootwrapperold/SpringBootWrapperOldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbt.springbootwrapperold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWrapperOldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
